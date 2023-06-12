040803a7e1c683142faeb8033a33ece509dd70d5
Vivian Zhou <vivianzhou1288@gmail.com>
2023-06-12 12:33:45 -0400

    Initial Commit

---
+++ EducationalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EducationalApp
+//
+//  Created by Vivian Zhou on 6/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
