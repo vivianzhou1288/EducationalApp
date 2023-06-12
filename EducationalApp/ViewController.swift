9151607fca95e38edcb7a8d7edee9a9e6708f0df
Vivian Zhou <vivianzhou1288@gmail.com>
2023-06-12 12:40:38 -0400

    Start Page

--- EducationalApp/ViewController.swift
+++ EducationalApp/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let nameLabel = UILabel()
+    let startButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .gray
+        
+        nameLabel.text = "Name"
+        nameLabel.font = .systemFont(ofSize: 30)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        startButton.setImage(UIImage(named: "startButton"), for: .normal)
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(startButton)
+        
+        setUpConstraints()
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
+        ])
+        
+        NSLayoutConstraint.activate([
+            startButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6),
+            startButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.27),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ])
     }
 
 
